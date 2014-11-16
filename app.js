b90681b13b5140c17746be9b3b8e304f506cdbc9
Simon Holmes <sholmes@liveperson.com>
2014-11-16 08:33:36 +0000

    Add UglifyJS for Angular app

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 require('./app_api/models/db');
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/app_server/views'));
 app.set('view engine', 'jade');
+
+var appClientFiles = [
+  'app_client/app.js',
+  'app_client/home/home.controller.js',
+  'app_client/common/services/geolocation.service.js',
+  'app_client/common/services/loc8rData.service.js',
+  'app_client/common/filters/formatDistance.filter.js',
+  'app_client/common/directives/ratingStars/ratingStars.directive.js'
+];
+var uglified = UglifyJS.minify(appClientFiles, { compress : false });
+
+fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Script generated and saved:", 'loc8r.min.js');
+  }
+});
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -4,7 +4,7 @@
     .module('loc8rApp')
     .service('loc8rData', loc8rData);
 
-  // loc8rData.$inject = ['$http'];
+  loc8rData.$inject = ['$http'];
   function loc8rData ($http) {
     var locationByCoords = function (lat, lng) {
       return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
